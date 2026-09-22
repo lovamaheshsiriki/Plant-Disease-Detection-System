20f738cf3927d9855c9cf196df3d44948e8ae2e5
lovamaheshsiriki <151653112+lovamaheshsiriki@users.noreply.github.com>
2026-09-22 21:51:26 +0530

    cores changing

--- app/backend/main.py
+++ app/backend/main.py
@@ -26,16 +26,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://plant-disease-detection-system-seven.vercel.app"
-],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # ============================================================
 # ROOT
