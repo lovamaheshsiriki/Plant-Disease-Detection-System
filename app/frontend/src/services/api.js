dd881c26834149be5aed1ddf570df4c050d0993c
lovamaheshsiriki <151653112+lovamaheshsiriki@users.noreply.github.com>
2026-09-22 19:47:11 +0530

    backend apiurl setting

--- app/frontend/src/services/api.js
+++ app/frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://plant-disease-detection-system-otkv.onrender.com";
 
 export async function predictPlantDisease(file) {
   const formData = new FormData();
